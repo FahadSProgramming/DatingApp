e9faa5f4a780c4ef1dc4c45ad0386f6e61629892
Fahad <Fahad@LAPTOP-48C88OCN>
2023-07-13 09:47:16 -0400

    Add project files.

---
+++ DatingApp.Persistence/DatingContext.cs
@@ -0,0 +1,14 @@
+﻿using DatingApp.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Persistence
+{
+    public class DatingContext : DbContext
+    {
+        public DatingContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
